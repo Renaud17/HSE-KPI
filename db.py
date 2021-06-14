024496cf1de95ac4e5b7f37b4af8e91103e20096
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-06-14 19:07:55 +0200

    Update db.py

--- db.py
+++ db.py
@@ -32,42 +32,42 @@ def create_table():
 # les donné que nous souhaitons récupré
 
 def add_Accueil(IDD,Chantier,NArrivant,Ninduction,Date):
-	c.execute('INSERT INTO Accueil(IDD,Chantier,NArrivant,Ninduction,Date) VALUES (?,?,?,?,?)ON CONFLICT DO NOTHING',(IDD,Chantier,NArrivant,Ninduction,Date))
+	c.execute('INSERT INTO Accueil(IDD,Chantier,NArrivant,Ninduction,Date) VALUES (?,?,?,?,?)',(IDD,Chantier,NArrivant,Ninduction,Date))
 	conn.commit()
 
 	
 def add_TBM(IDD,Chantier,NChantier,NTBM,Date):
-	c.execute('INSERT INTO TBM(IDD,Chantier,NChantier,NTBM,Date) VALUES (?,?,?,?,?)ON CONFLICT DO NOTHING',(IDD,Chantier,NChantier,NTBM,Date))
+	c.execute('INSERT INTO TBM(IDD,Chantier,NChantier,NTBM,Date) VALUES (?,?,?,?,?)',(IDD,Chantier,NChantier,NTBM,Date))
 	conn.commit()	
 
 	
 def add_NC(IDD,Chantier,NCR,FNCR,NCC,FNCC,Date):
-	c.execute('INSERT INTO NC(IDD,Chantier,NCR,FNCR,NCC,FNCC,Date) VALUES (?,?,?,?,?,?,?)ON CONFLICT DO NOTHING',(IDD,Chantier,NCR,FNCR,NCC,FNCC,Date))
+	c.execute('INSERT INTO NC(IDD,Chantier,NCR,FNCR,NCC,FNCC,Date) VALUES (?,?,?,?,?,?,?)',(IDD,Chantier,NCR,FNCR,NCC,FNCC,Date))
 	conn.commit()	
 
 
 def add_Changements(IDD,Chantier,NCH,FNCH,NCHC,FNCHC,Date):
-	c.execute('INSERT INTO Changements(IDD,Chantier,NCH,FNCH,NCHC,FNCHC,Date) VALUES (?,?,?,?,?,?,?)ON CONFLICT DO NOTHING',(IDD,Chantier,NCH,FNCH,NCHC,FNCHC,Date))
+	c.execute('INSERT INTO Changements(IDD,Chantier,NCH,FNCH,NCHC,FNCHC,Date) VALUES (?,?,?,?,?,?,?)',(IDD,Chantier,NCH,FNCH,NCHC,FNCHC,Date))
 	conn.commit()	
 
 	
 def add_Anomalies(IDD,Chantier,NA,FNA,NAC,FNAC,Date):
-	c.execute('INSERT INTO Anomalies(IDD,Chantier,NA,FNA,NAC,FNAC,Date) VALUES (?,?,?,?,?,?,?)ON CONFLICT DO NOTHING',(IDD,Chantier,NA,FNA,NAC,FNAC,Date))
+	c.execute('INSERT INTO Anomalies(IDD,Chantier,NA,FNA,NAC,FNAC,Date) VALUES (?,?,?,?,?,?,?)',(IDD,Chantier,NA,FNA,NAC,FNAC,Date))
 	conn.commit()	
 
 
 def add_JSA(IDD,Chantier,NAct,NJSA,Date):
-	c.execute('INSERT INTO JSA(IDD,Chantier,NAct,NJSA,Date) VALUES (?,?,?,?,?)ON CONFLICT DO NOTHING',(IDD,Chantier,NAct,NJSA,Date))
+	c.execute('INSERT INTO JSA(IDD,Chantier,NAct,NJSA,Date) VALUES (?,?,?,?,?)',(IDD,Chantier,NAct,NJSA,Date))
 	conn.commit()
 
 	
 def add_Incident_Accident(IDD,Chantier,NInc,AAA,ASA,AT,NJP,Date):
-	c.execute('INSERT INTO Incident_Accident(IDD,Chantier,NInc,AAA,ASA,AT,NJP,Date) VALUES (?,?,?,?,?,?,?,?)ON CONFLICT DO NOTHING',(IDD,Chantier,NInc,AAA,ASA,AT,NJP,Date))
+	c.execute('INSERT INTO Incident_Accident(IDD,Chantier,NInc,AAA,ASA,AT,NJP,Date) VALUES (?,?,?,?,?,?,?,?)',(IDD,Chantier,NInc,AAA,ASA,AT,NJP,Date))
 	conn.commit()
 
 	
 def add_Audit(IDD,Chantier,AC,VC,NEU,SMPAR,NPR,IE,Date):
-	c.execute('INSERT INTO Audit(IDD,Chantier,AC,VC,NEU,SMPAR,NPR,IE,Date) VALUES (?,?,?,?,?,?,?,?,?)ON CONFLICT DO NOTHING',(IDD,Chantier,AC,VC,NEU,SMPAR,NPR,IE,Date))
+	c.execute('INSERT INTO Audit(IDD,Chantier,AC,VC,NEU,SMPAR,NPR,IE,Date) VALUES (?,?,?,?,?,?,?,?,?)',(IDD,Chantier,AC,VC,NEU,SMPAR,NPR,IE,Date))
 	conn.commit()
 	
 #=========================================================================================	
