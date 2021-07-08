98ef92a905fd2a01a2e328937cf31a5de9a60b6d
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-08 13:31:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -244,21 +244,26 @@ def main():
 			}
 			</script>
 			""")
-        html_temp = """
-		<div style="background-color:#464e5f;padding:10px;border-radius:10px;margin:3px;">
-		<h1 style="font-family: 'BadaBoom BB', sans-serif;color:white;text-align:center;"><b>HSE KPI RECORDER & HSEbot</b></h1>
-		</div>
-		"""
-	
-        #components.html(html_temp)
-        st.markdown(html_temp, unsafe_allow_html = True)
-        st.header("**Pourquoi HSE KPI RECORDER et HSEbot?**")
+	html_temp = """
+                            <div style="background-color:#87CEFA ;padding:10px">
+                            <h2 style="color:white;text-align:center;">Alternatives of Streamlit</h2>
+                            </div>
+                            """
+        st.markdown(html_temp, unsafe_allow_html=True)
+        st.markdown("**Considering scalability as the main concern, we have Flask and FastAPI which have a complete edge over streamlit**")
+
+        st.subheader("**Flask**")
+        st.markdown("✨ **It is a Python-based framework that allows to hook up websites with less amount of code. We can create a small-scale website with this as it allows customization at every step(highly customizable)**")
+        st.markdown("✨ **Being a minimalistic package, only core components are bundled with this and all other extensions require explicit setup**")
+        st.markdown("✨ **There are some issues with Flask such as slow nature, no async, and web sockets support that can speed up the processes, and finally no automated docs generation system. We need to manually design the user interface for the usage and examples of the API**")
+        st.markdown("✨ **All these issues are resolved in the new framework FastAPI **")
+	st.header("**Pourquoi HSE KPI RECORDER et HSEbot?**")
 	st.markdown("✨ ** Elle est une application d'analyse et de suivi des indicateurs de performance HSE dotée d'une intelligence artificielle pour identifier et prevenir les risques et dangers au travail.**")
 	st.markdown("✨ **Vous pouvez ajouter;modifier;supprimer et visualiser vos données avec des graphes**")
 	st.markdown("✨ **Vous pouvez aussi téléchager vos données selon l'intervalle de date souhaitée**")
 	st.markdown("✨ **HSEbot vous permet de discuter de manière inter-active avec une intelligence artificielle qui vous donne des conseils de prévention sur les risques au chantier.**")
         
-        image_BOT = """
+	image_BOT = """
 		<center><img src="https://www.trainingjournal.com/sites/www.trainingjournal.com/files/styles/original_-_local_copy/entityshare/23924%3Fitok%3DKw_wPH9G"  alt="HSEBOT" height="150" width="200"></center>
 		"""
         
